3b54a96e9b7ea8d6541b8898a3243897d0b81b23
Jakub Wilga <wilga327@wp.pl>
2023-04-07 10:11:19 +0200

    ActivatePages and add sass changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,60 @@
-import { settings} from './settings.js';
+import { select, settings, classNames} from './settings.js';
 import Product from './components/Product.js';
 
 const app = {
 
+
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        thisApp.activatePage(id);
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    for (let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    for (let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
   initProduct: function(){
     const thisApp = this;
+
+    
 
     for (let productData in thisApp.data.products){
       new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
@@ -29,6 +79,7 @@ const app = {
   init: function() {
     const thisApp = this;
     thisApp.initData();
+    thisApp.initPages();
   },
 };
 
